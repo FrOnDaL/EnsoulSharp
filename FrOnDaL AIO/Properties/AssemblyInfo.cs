8bd46580311f573887964b7108b91e1749560cac
FrOnDaL <frondalbolu@gmail.com>
2019-06-30 16:25:23 +0300

    Update

--- FrOnDaL AIO/Properties/AssemblyInfo.cs
+++ FrOnDaL AIO/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FrOnDaL AIO")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("FrOnDaL")]
+[assembly: AssemblyDescription("FrOnDaL")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FrOnDaL AIO")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
